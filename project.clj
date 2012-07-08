a1f6f5d896bbe2bbe2d7119947d5ec44f0852e38
TANAKA KENTA <tanaka.kenta.elso0861@gmail.com>
2012-07-08 16:20:10 +1000

    dep. solv

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject my-dictionary "0.1.0-SNAPSHOT"
+  :description "A tiny web application for requesting Dictionary.com web api"
+  :url "http://"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.3.0"]
+    [org.clojure/tools.logging "0.2.3"]
+    [log4j/log4j "1.2.16"]
+    [log4j/apache-log4j-extras "1.1"]
+    [clj-http "0.4.3"]
+    [clj-json "0.5.0"]
+    [clj-xpath "1.3.0"]])
+   
