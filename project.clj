c4c21697732054d4120df71596db233a26338078
TANAKA KENTA <tanaka.kenta.elso0861@gmail.com>
2012-08-18 13:32:59 +1000

    upgrade lbraries

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.3.0"]
+    [org.clojure/clojure "1.4.0"]
     [org.clojure/tools.logging "0.2.3"]
     [log4j/log4j "1.2.16"]
     [log4j/apache-log4j-extras "1.1"]
     [clj-http "0.4.3"]
     [clj-json "0.5.0"]
     [clj-xpath "1.3.0"]
-    [compojure "1.1.0"]
+    [compojure "1.1.1"]
     [ring/ring-jetty-adapter "1.1.1"]]
   :ring {:handler my-dictionary.core/interface-for-client}
 )
